6611521a48b4ae644430064f473d6d78567be123
Orlando <109116795+osirais@users.noreply.github.com>
2025-07-01 13:36:38 -0500

    temp fix

--- middleware.ts
+++ middleware.ts
@@ -5,23 +5,23 @@ export async function middleware(request: NextRequest) {
   const response = await updateSession(request);
 
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const csp = `
-    default-src 'self';
-    script-src 'nonce-${nonce}' 'strict-dynamic' ${
-    process.env.NODE_ENV === "production" ? "unsafe-eval" : `'unsafe-eval'`
-  };
-    style-src 'self' 'unsafe-inline';
-    connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL};
-    img-src 'self' blob: data:;
-    object-src 'none';
-    base-uri 'none';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
-    .replace(/\s{2,}/g, " ")
-    .trim();
+  //   const csp = `
+  //     default-src 'self';
+  //     script-src 'nonce-${nonce}' 'strict-dynamic' ${
+  //     process.env.NODE_ENV === "production" ? "unsafe-eval" : `'unsafe-eval'`
+  //   };
+  //     style-src 'self' 'unsafe-inline';
+  //     connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL};
+  //     img-src 'self' blob: data:;
+  //     object-src 'none';
+  //     base-uri 'none';
+  //     frame-ancestors 'none';
+  //     upgrade-insecure-requests;
+  // `
+  //     .replace(/\s{2,}/g, " ")
+  //     .trim();
 
-  response.headers.set("Content-Security-Policy", csp);
+  //   response.headers.set("Content-Security-Policy", csp);
   response.headers.set("x-nonce", nonce);
 
   return response;
