2c0ac814713672a9fef231d44c4436dcae331eb4
Orlando <109116795+osirais@users.noreply.github.com>
2025-07-01 13:04:01 -0500

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,29 @@ import { updateSession } from "@/lib/supabase/middleware";
 import { type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const csp = `
+    default-src 'self';
+    script-src 'nonce-${nonce}' 'strict-dynamic' ${
+    process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+  };
+    style-src 'self' 'unsafe-inline';
+    connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL};
+    img-src 'self' blob: data:;
+    object-src 'none';
+    base-uri 'none';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  response.headers.set("Content-Security-Policy", csp);
+  response.headers.set("x-nonce", nonce);
+
+  return response;
 }
 
 export const config = {
