9fbc9393e070d33e6c5031ede6f8b3b87fc43fc7
maxhu08 <contact@maxhu.dev>
2025-07-01 12:59:58 -0700

    use handle image upload

--- app/uploadthing-test/page.tsx
+++ app/uploadthing-test/page.tsx
@@ -1,22 +1,31 @@
-"use client";
+'use client'
 
-import { UploadButton } from "@/utils/uploadthing";
+import { createClient } from '@/lib/supabase/server'
+import { handleImageUpload } from '@/utils/handle-image-upload'
+import { UploadButton } from '@/utils/uploadthing'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <UploadButton
         endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
+        onClientUploadComplete={async (res) => {
+          console.log('Files:', res)
+
+          const supabase = await createClient()
+          const { data, error } = await supabase.auth.getUser()
+
+          if (error || !data?.user) return
+
+          await handleImageUpload(res, data.user.id)
+
+          alert('Upload Completed')
         }}
         onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
+          alert(`ERROR! ${error.message}`)
         }}
       />
     </main>
-  );
+  )
 }
+
