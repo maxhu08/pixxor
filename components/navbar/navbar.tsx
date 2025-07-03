498b131033a4d030785fe0e751807a34ab6819f8
Orlando <109116795+osirais@users.noreply.github.com>
2025-07-03 17:37:56 -0500

    tweak navbar width

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,12 +6,13 @@ import type { FC } from "react";
 export const Navbar: FC = () => {
   return (
     <nav className="border-b-foreground/10 bg-background sticky top-0 left-0 z-50 flex h-16 w-full justify-center border-b">
-      <div className="flex w-full max-w-7xl items-center justify-between px-5 py-3 text-sm">
+      <div className="container flex w-full items-center justify-between px-8 py-3 text-sm">
         <div className="flex items-center gap-5 font-semibold">
           <Link
             href="/"
             className="flex items-center gap-2 text-lg font-bold tracking-tight transition-opacity hover:opacity-80"
           >
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
             <Image className="size-6" />
             <span className="font-mono">pixxor</span>
           </Link>
