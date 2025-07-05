1c5d11328f61ab49ab098771e2bc25873473a0ee
maxhu08 <contact@maxhu.dev>
2025-07-04 17:10:58 -0700

    change text

--- components/albums/create-album-button.tsx
+++ components/albums/create-album-button.tsx
@@ -17,7 +17,7 @@ export function CreateAlbumButton() {
     >
       <div className="flex items-center">
         <PlusCircle className="mr-2 h-4 w-4" />
-        Create Your First Album (WIP)
+        Create Album
       </div>
     </Button>
   );
