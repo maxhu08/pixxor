8ed7d99cbc1c999083f1479a63b908c101fe8aa6
Orlando <109116795+osirais@users.noreply.github.com>
2025-07-06 12:07:15 -0700

    add auth route

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,35 @@
+// The client you created from the Server-Side Auth instructions
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  let next = searchParams.get("next") ?? "/";
+  if (!next.startsWith("/")) {
+    // if "next" is not a relative URL, use the default
+    next = "/";
+  }
+
+  if (code) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      const redirectTo = "/onboarding";
+
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${redirectTo}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${redirectTo}`);
+      } else {
+        return NextResponse.redirect(`${origin}${redirectTo}`);
+      }
+    }
+  }
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
